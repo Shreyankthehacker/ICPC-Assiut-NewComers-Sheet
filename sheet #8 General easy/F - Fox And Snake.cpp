cf3bd0785418e2ae678e2e329d0235f5560babf2
Shreyank isiri <76459138+Shreyankthehacker@users.noreply.github.com>
2024-06-11 12:30:00 +0530

    Update F - Fox And Snake.cpp
    another easy approach 
    its my first contribution ........ thanks

--- sheet #8 General easy/F - Fox And Snake.cpp
+++ sheet #8 General easy/F - Fox And Snake.cpp
@@ -1,36 +1,20 @@
-#include <iostream>
-#include <algorithm>
-#include <cctype>
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <string>
-#include<cmath>
-
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
-int main(void){
-    int r,c;
-    cin>>r>>c;
-
-    int end=0;
-    for (int i = 0; i<r; i++) {
-        if (i%2 ==0 ) {
-            for (int i =0; i<c; i++) cout <<"#";
-        } else {
-            end++;
-            for (int j=0; j<c; j++) {
-                if ( (end %2 ==1 && j == c-1) || (end %2 ==0 && j ==0) ){
-                    cout <<"#";
-                } else cout <<".";
-            }
-        }
-        cout <<endl;
-    }
+int main(){
+int rows,cols;
+cin>>rows>>cols;
+for(int i=1;i<=rows;i++){
+	if(i%4==2){
+		for(int j=1;j<cols;j++)cout<<".";
+		cout<<"#";
+	}
+	else if(i%4==0){
+		cout<<"#";
+		for(int j=2;j<=cols;j++)cout<<".";
+	}
+	else{
+		for(int j=1;j<=cols;j++)cout<<"#";
+	}
+	cout<<endl;
 }
-
-
-
-
-
+}
